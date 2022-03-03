97820aecabb21ebb2cef58568669952837dd9b03
saviosaju10 <100836304+saviosaju10@users.noreply.github.com>
2022-03-03 16:08:12 +0530

    Update contact-form-handler.php
    updated the php code

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,17 +1,15 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $POST['message'];
-
-    $to = "saviosaju10@gmail.com";
-    $subject = "Mail From website";
-    $text = "Nmae = ". $name . "\r\nEmail = " . $email . "\r\nMessage =" . $message;
-    $headers = "From: noreply@sandx.com" . "\r\n" .
-
-    if($email!=NULL){
-      mail($to,$subject,$txt,$headers);
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $POST['message'];
+$to = "saviosaju10@gmail.com";
+$subject = "Mail From website";
+$text = "Nmae = ". $name . "\r\nEmail = " . $email . "\r\nMessage =" . $message;
+$headers = "From: noreply@sandx.com" . "\r\n" .
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
     }
-    //Redirect
-    header("Location:index.html")
+//Redirect 
+header("Location:index.html")
 
 ?>
