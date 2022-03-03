2c540bd956d67be264db3179779940f69f67273c
saviosaju10 <100836304+saviosaju10@users.noreply.github.com>
2022-03-03 15:58:54 +0530

    Add files via upload
    ADDES NEW FILES

---
+++ contact-form-handler.php
@@ -0,0 +1,17 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $POST['message'];
+
+    $to = "saviosaju10@gmail.com";
+    $subject = "Mail From website";
+    $text = "Nmae = ". $name . "\r\nEmail = " . $email . "\r\nMessage =" . $message;
+    $headers = "From: noreply@sandx.com" . "\r\n" .
+
+    if($email!=NULL){
+      mail($to,$subject,$txt,$headers);
+    }
+    //Redirect
+    header("Location:index.html")
+
+?>
